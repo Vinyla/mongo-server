d2e16640306a6fdb52bad7d454e3277cda6dfaa9
Vinyla <irma.rustemovic@gmail.com>
2021-04-21 23:48:36 +0200

    routes defined

---
+++ src/router/course.router.js
@@ -0,0 +1,8 @@
+import express from 'express';
+import ctrl from '../controllers/courses.controller';
+
+const router = express.Router();
+
+router.route('./courses').get(ctrl.list);
+
+export default router;
---
+++ src/router/student.router.js
@@ -0,0 +1,8 @@
+import express from 'express';
+import ctrl from '../controllers/students.controller';
+
+const router = express.Router();
+
+router.route('./students').get(ctrl.list);
+
+export default router;
